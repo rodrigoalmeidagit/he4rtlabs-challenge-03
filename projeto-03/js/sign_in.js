de118b9534974370cc241303b6c6e21010b34942
Rodrigo Almeida <rodrigoalmeidagit@gmail.com>
2021-04-07 23:26:17 -0300

    included class to js

--- projeto-03/js/sign_in.js
+++ projeto-03/js/sign_in.js
@@ -1,5 +1,8 @@
-( function( win, doc ) {
+(function (win, doc) {
   'use strict';
 
+  var $button = document.querySelector('[data-js="sign-in"]');
+  var $sign = document.querySelector('[data-js="sign-up"]');
 
-} )( window, document );
+
+})(window, document);
